472fb08a4f4cebe3d744a03dbde19a18e5bf4085
penugonda suraj <penugonda suraj>
2017-02-16 00:47:55 +0530

    tester

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ server.listen(process.env.port||process.env.PORT||3978,function(){
     console.log("%s listening to %s",server.name, server.url);
 });
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "351e1a98-1bd6-4865-8d64-61ab3902e4b9",
+    appPassword: 'yPBwj4iMyj8fbRYRyqCjmAU'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
