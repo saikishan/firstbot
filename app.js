b08ae663f2cdc2d43e44b57d44b4d360b780221c
penugonda suraj <penugonda suraj>
2017-02-16 00:36:44 +0530

    tester

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var server = restify.createServer();
+//status tester
 server.listen(process.env.port||process.env.PORT||3978,function(){
     console.log("%s listening to %s",server.name, server.url);
 });
