8d523a867654b7d507b4cde0a691c4179da8c896
saikishan <saikishan2008@gmail.com>
2017-02-16 11:19:34 +0530

    Update app.js
    suraj

--- app.js
+++ app.js
@@ -11,6 +11,77 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-bot.dialog('/',function(session){
-    session.send("Hello World");
+var disha = new builder.IntentDialog();
+bot.dialog('/', disha);
+
+disha.onDefault([
+     function (session,args,next) {
+        if (!session.userData.name) {
+           session.send("hey, I am Disha!");
+            session.beginDialog('/profile');
+
+        } else {
+            next();
+        }
+    },
+    function (session) {
+        session.send('how can i help you %s', session.userData.name);
+    }
+]);
+
+disha.matches(/^i love you/i, function(session){
+    session.send('aww... I Love You too...%s',session.userData.name);
 });
+
+disha.matches(/^change user/,[ function(session){
+    session.beginDialog('/profile');
+},
+function(session){
+    session.send('user name changed to....%s', session.userData.name);
+}
+]);
+
+disha.matches(/^where is this event?/i, function (session) {
+    session.send('VIT , Bhimavaram');
+});
+
+disha.matches(/^when is the event?/i,function(session){
+    session.send("on 22nd and 23rd of this month");
+});
+
+disha.matches(/^who is the coordinator?/i,[
+    function(session){
+    builder.Prompts.text(session,"can you be more specific about the department");
+},
+function(session,results){
+    switch(results.response){
+        case "cse" :session.send("Mr.B.Sumanth");
+                        break;
+        
+        case "ece" :session.send("Mr.x");
+                        break;  
+        
+        case "eee" :session.send("Mr.y");
+                        break;           
+        
+        case "mech" :session.send("Mr.p");
+                        break;              
+         default : session.send("foo bar");                                    
+    }
+}
+]);
+
+disha.matches(/^do you have a boyfriend?/i, function (session) {
+    session.send('NO personal questions please...');
+});
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'what is your name..?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.send('hiee %s', session.userData.name);
+        session.endDialog();
+    }
+]);
